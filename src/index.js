f3310d63035f15abd218f2899f30449bbcf80da6
ErgonComun <ergon.comunicacao.atendimento@gmail.com>
2026-03-03 16:08:25 -0300

    epecial offer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = require('./middleware/auth');
 const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_fallback');
-const STRIPE_PRICE_ID = process.env.STRIPE_PRICE_ID || 'price_1T6y0DARnJXQ6e7FeKVYNwcO';
+const STRIPE_PRICE_ID = process.env.STRIPE_PRICE_ID || 'price_1T6yG3ARnJXQ6e7F7sgi0wNK';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
